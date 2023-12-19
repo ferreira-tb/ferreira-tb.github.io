f911e6d909e781942183fdb4570bf542224b52d1
Andrew Ferreira <andrew.shien2@gmail.com>
2023-12-19 15:01:12 -0300

    ci: downgrade action version

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import dev from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    minify: false
-  }
+  plugins: [vue(), dev()]
 });
