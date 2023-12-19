4fb97a8d08db6c90a7d2d269b6852df3b730abbf
Andrew Ferreira <andrew.shien2@gmail.com>
2023-12-19 16:20:53 -0300

    feat: add manatsu

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+import 'manatsu/style';
 import './assets/main.scss';
 import { createApp } from 'vue';
+import { createManatsu } from 'manatsu';
 import App from './App.vue';
 
 const app = createApp(App);
+const manatsu = createManatsu({
+  darkMode: 'auto'
+});
+
+app.use(manatsu);
 app.mount('#app');
