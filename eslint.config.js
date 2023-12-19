f9b8bf44b56ee710330ce26a6649c0942dd33a2f
Andrew Ferreira <andrew.shien2@gmail.com>
2023-12-19 14:19:04 -0300

    chore: initial commit

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import config from '@tb-dev/eslint-config';
+
+export default config({
+  vue: true,
+  project: ['tsconfig.json'],
+  overrides: {
+    vue: {
+      'vue/block-lang': [
+        'error',
+        {
+          script: {
+            lang: 'ts'
+          },
+          style: {
+            lang: 'scss',
+            allowNoLang: false
+          }
+        }
+      ]
+    }
+  }
+});
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div>@ferreira-tb</div>
+  </main>
+</template>
+
+<style scoped lang="scss"></style>
