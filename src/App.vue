9b12b66ca10f428b3f70ae92278ad0f4ac88939b
Andrew Ferreira <andrew.shien2@gmail.com>
2023-12-29 19:30:46 -0300

    chore: bump deps

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { type IconLinkProps, MNavbar, MScaffold } from 'manatsu';
+
+const socialLinks: IconLinkProps[] = [
+  {
+    icon: 'github',
+    to: 'https://github.com/ferreira-tb'
+  }
+];
+</script>
 
 <template>
-  <main>
-    <div>@ferreira-tb</div>
-  </main>
+  <MScaffold navbar>
+    <template #navbar>
+      <MNavbar :social-links="socialLinks" />
+    </template>
+    <main>
+      <div class="placeholder">@ferreira-tb</div>
+    </main>
+  </MScaffold>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.placeholder {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 3rem;
+}
+</style>
