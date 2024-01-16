0c83276cf2d9ba46991e948a6bfb026bc254c369
Andrew Ferreira <andrew.shien2@gmail.com>
2024-01-16 19:56:29 -0300

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,5 @@ import config from '@tb-dev/eslint-config';
 
 export default config({
   vue: true,
-  project: ['tsconfig.json'],
-  overrides: {
-    vue: {
-      'vue/block-lang': ['error', { script: { lang: 'ts' } }]
-    }
-  }
+  project: ['tsconfig.json']
 });
