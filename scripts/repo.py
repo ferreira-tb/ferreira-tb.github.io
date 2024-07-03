ff69b6130edfc1441db5417ba0eb68d425222755
Andrew Ferreira <andrew.shien2@gmail.com>
2024-07-03 15:17:57 -0300

    chore: deploy

--- scripts/repo.py
+++ scripts/repo.py
@@ -5,19 +5,14 @@ from os import path
 from subprocess import CalledProcessError
 
 
-def get_info(name: str, extra_langs: list[str] = []):
-    fields = "name,description,url,languages,primaryLanguage,updatedAt"
-
-    try:
-        output = subprocess.run(
-            ["gh", "repo", "view", name, "--json", fields],
-            capture_output=True,
-            check=True,
-            text=True,
-        )
-    except CalledProcessError as err:
-        print(err.stderr)
-        raise
+def get(name: str, extra_langs: list[str] = []):
+    fields = "name,description,url,languages,primaryLanguage,updatedAt,stargazerCount"
+    output = subprocess.run(
+        ["gh", "repo", "view", name, "--json", fields],
+        capture_output=True,
+        check=True,
+        text=True,
+    )
 
     info = json.loads(output.stdout)
     info["extraLanguages"] = extra_langs
@@ -26,9 +21,9 @@ def get_info(name: str, extra_langs: list[str] = []):
 
 
 repos = [
-    get_info("ferreira-tb/kotori"),
-    get_info("ferreira-tb/miho"),
-    get_info("ferreira-tb/tauri-plugin-prevent-default"),
+    get("ferreira-tb/kotori"),
+    get("ferreira-tb/miho"),
+    get("ferreira-tb/tauri-plugin-prevent-default"),
 ]
 
 file_path = path.join(os.getcwd(), "public/data/repos.json")
