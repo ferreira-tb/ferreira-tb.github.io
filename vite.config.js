407c6b52e4e94c9724378df23f1a767003a0e401
Andrew Ferreira <andrew.shien2@gmail.com>
2025-03-20 22:17:48 -0300

    chore: deploy

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
-import tailwind from 'tailwindcss';
+import tailwind from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import autoprefixer from 'autoprefixer';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  plugins: [vue()],
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
-  },
+  plugins: [vue(), tailwind()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('src', import.meta.url)),
