52b791a43f257a6cd3526a3c3b769273fc75aa6d
Andrew Ferreira <andrew.shien2@gmail.com>
2026-04-24 12:26:24 -0300

    chore: deploy

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import '@/style.css';
+import '@/assets/style.css';
 import App from '@/App.vue';
 import { createApp } from 'vue';
 import { router } from '@/router';
 
-createApp(App).use(router).mount('#app');
+createApp(App)
+  .use(router)
+  .mount('#app');
